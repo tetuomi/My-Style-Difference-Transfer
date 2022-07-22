5ef27774d1f742f03638b93aef53555db5799f47
tetuomi <kondo.tetta851@mail.kyutech.jp>
2022-07-22 15:03:52 +0900

    Add image transform to toBGR and normalization

--- train_vgg.py
+++ train_vgg.py
@@ -1,6 +1,7 @@
 import random
 from os import path
 
+import PIL
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -10,10 +11,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
-from torch.optim import lr_scheduler
+from torchvision import transforms
+# from torch.optim import lr_scheduler
 
-from utility.vgg_network_with_top import VGG, Model
-from utility.utility import load_mono_images
+from utility.vgg_network_with_top import VGG
 
 
 def make_path_list():
@@ -24,7 +25,6 @@ def make_path_list():
     test_list = data_df[data_df['type'] == 'test']['dirname'].to_list()
 
     char_list = [chr(c_code) for c_code in range(ord('A'), ord('Z')+1)]
-    # train_list = [[path.join('../font2img/image', d, char+'.png') for char in char_list] for d in ['AbhayaLibre-ExtraBold']]
     train_list = [[path.join('../font2img/image', d, char+'.png') for char in char_list] for d in train_list]
     val_list = [[path.join('../font2img/image', d, char+'.png') for char in char_list] for d in val_list]
     test_list = [[path.join('../font2img/image', d, char+'.png') for char in char_list] for d in test_list]
@@ -41,33 +41,43 @@ def make_path_list():
 
 
 class LoadDataset(data.Dataset):
-    def __init__(self, file_list, device, img_size):
+    def __init__(self, file_list, transform):
         self.file_list = file_list
-        self.device = device
-        self.img_size = img_size
+        self.transform = transform
+        self.invert = True
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, index):
-        invert = 1
         filepath = self.file_list[index]
-        img = load_mono_images(filepath, self.img_size, self.device, invert)
+        img = PIL.Image.open(filepath).convert('RGB')
+        if self.invert:
+            img = PIL.ImageOps.invert(img)
+        img_tensor = self.transform(img)
         alphabet = path.basename(filepath)[:-4]
         label = ord(alphabet) - ord('A')
 
-        return img, label
+        return img_tensor, label
 
 
-def make_dataloader(device):
+def make_dataloader():
     img_size = 256
-    batch_size = 128
+    batch_size = 64
+
+    transform = transforms.Compose([transforms.Resize((img_size,img_size)),
+                            transforms.ToTensor(),
+                            transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
+                            transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
+                                                    std=[1,1,1]),
+                            transforms.Lambda(lambda x: x.mul_(255)),
+                        ])
 
     train_path, val_path, test_path = make_path_list()
 
-    train_dataset = LoadDataset(train_path, device, img_size)
-    val_dataset = LoadDataset(val_path, device, img_size)
-    test_dataset = LoadDataset(test_path, device, img_size)
+    train_dataset = LoadDataset(train_path, transform)
+    val_dataset = LoadDataset(val_path, transform)
+    test_dataset = LoadDataset(test_path, transform)
 
     train_dataloader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -80,31 +90,19 @@ def train(device):
     layers = ['fc3']
     epochs = 100
     n_classes = 26
-    vgg_filename = 'resnet18.pth'
+    vgg_filename = 'resnet_BGR.pth'
     device = 'cuda:0' if torch.cuda.is_available else 'cpu'
 
     # vgg = VGG(n_classes)
-
-    # vgg = Model()
-
-    # vgg = torch.hub.load('pytorch/vision:v0.10.0', 'vgg19', init_weights=False)
-
     from torchvision import models
-    # vgg = models.vgg19()
-    # vgg.features[0] = nn.Conv2d(1, 64, kernel_size=3, padding=1)
-    # vgg.avgpool = nn.AdaptiveAvgPool2d((8, 8))
-    # vgg.classifier[0] = nn.Linear(512*8*8, 4096)
-    # vgg.classifier[6] = nn.Linear(4096, 26)
-
     vgg = models.resnet18(pretrained=False)
-    vgg.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
     vgg.fc = nn.Linear(512, 26)
 
     optimizer = optim.Adam(vgg.parameters(), lr=0.001)
     # optimizer = optim.SGD(vgg.parameters(), lr=0.001, momentum=0.9)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
     criterion = nn.CrossEntropyLoss()
-    dataloader = make_dataloader(device)
+    dataloader = make_dataloader()
 
     vgg = vgg.to(device)
 
@@ -126,7 +124,7 @@ def train(device):
             epoch_corrects = 0
 
             for inputs, labels in tqdm(dataloader[phase]):
-                # inputs = inputs.to(device)
+                inputs = inputs.to(device)
                 labels = labels.to(device)
 
                 optimizer.zero_grad()
@@ -134,18 +132,17 @@ def train(device):
                 with torch.set_grad_enabled(phase == 'train'):
                     # outputs = vgg(inputs, layers)[0]
                     outputs = vgg(inputs)
-                    # print(phase, 'fc3', outputs)
 
-                    _, preds = torch.max(outputs, 1)
-                    # print(phase, 'preds', preds, 'labels', labels)
+                    probs = nn.Softmax(dim=1)(outputs)
+                    _, preds = torch.max(probs, 1)
                     loss = criterion(outputs, labels)
 
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
 
-                epoch_loss += loss.item() * inputs.size(0)
-                epoch_corrects += torch.sum(preds == labels.data)
+                    epoch_loss += loss.item() * inputs.size(0)
+                    epoch_corrects += torch.sum(preds == labels.data)
 
             epoch_loss = epoch_loss / len(dataloader[phase].dataset)
             epoch_acc = epoch_corrects.double() / len(dataloader[phase].dataset)
