e5e1c79170ad2f78c36aea15794999a9cb4d12be
tetuomi <kondo.tetta851@mail.kyutech.jp>
2022-07-22 15:08:31 +0900

    Change input channels to 3 channels

--- utility/vgg_network_with_top.py
+++ utility/vgg_network_with_top.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class VGG(nn.Module):
-    def __init__(self, n_classes, pool='max'):
+    def __init__(self, n_classes=26, pool='max'):
         super(VGG, self).__init__()
         # feature extractor
-        self.conv1_1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
@@ -96,43 +96,3 @@ class VGG(nn.Module):
         out['fc3'] = self.fc3(out_fc2)
 
         return [out[key] for key in out_keys]
-
-
-class Model(nn.Module):
-
-    def __init__(self):
-        # スーパークラス（Module クラス）の初期化メソッドを実行
-        super().__init__()
-
-        self.c0 = nn.Conv2d(in_channels=1,    # 入力は3チャネル
-                            out_channels=16,  # 出力は16チャネル
-                            kernel_size=3,    # カーネルサイズは3*3
-                            stride=2,         # 1pix飛ばしでカーネルを移動
-                            padding=1)        # 画像の外側1pixを埋める
-
-        self.c1 = nn.Conv2d(in_channels=16,   # 入力は16チャネル
-                            out_channels=32,  # 出力は32チャネル
-                            kernel_size=3,    # カーネルサイズは3*3
-                            stride=2,         # 1pix飛ばしでカーネルを移動
-                            padding=1)        # 画像の外側1pixを埋める
-
-        self.c2 = nn.Conv2d(in_channels=32,   # 入力は32チャネル
-                            out_channels=64,  # 出力は64チャネル
-                            kernel_size=3,    # カーネルサイズは3*3
-                            stride=2,         # 1pix飛ばしでカーネルを移動
-                            padding=1)        # 画像の外側1pixを埋める
-
-        self.bn0 = nn.BatchNorm2d(num_features=16)   # c0用のバッチ正則化
-        self.bn1 = nn.BatchNorm2d(num_features=32)   # c1用のバッチ正則化
-        self.bn2 = nn.BatchNorm2d(num_features=64)   # c2用のバッチ正則化
-
-        self.fc = nn.Linear(in_features=64 * 32 * 32,   # 入力サイズ
-                            out_features=26)             # 各クラスに対応する4次元のベクトルに変換
-
-    def forward(self, x): # 入力から出力を計算するメソッドを定義
-        h = F.relu(self.c0(x))
-        h = F.relu(self.c1(h))
-        h = F.relu(self.c2(h))
-        h = h.view(-1, 64 * 32 * 32)
-        y = self.fc(h)     # 全結合層
-        return y
